dddb57f3fec5c03dd7adee0aabee513d81e3578a
root <root@ns542209.ip-144-217-255.net>
2017-10-27 00:16:43 -0400

    Changed the gas cost

--- truffle.js
+++ truffle.js
@@ -1,6 +1,8 @@
 module.exports = {
   networks: {
     development: {
+      gas: 4012388,
+      gasPrice: 450000000000,
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
