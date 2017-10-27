46c5305d1793810fa559a53a87e8eb8a540865e6
root <root@ns542209.ip-144-217-255.net>
2017-10-27 00:14:56 -0400

    Changes deployed code

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,24 +3,23 @@ var MiniMeTokenFactory = artifacts.require("./MiniMeTokenFactory.sol");
 var ProfitSharing = artifacts.require("./ProfitSharing.sol");
 
 //NB - change from mocked contracts to non-mocked version
-var SpectreUtilityToken = artifacts.require("./SpectreUtilityTokenMock.sol");
-var SpectreDividendToken = artifacts.require("./SpectreDividendTokenMock.sol");
-var SpectreSubscriberToken = artifacts.require("./SpectreSubscriberTokenMock.sol");
-
-//NB - set real addresses
-var OWNER = web3.eth.accounts[0];
-var WALLET = web3.eth.accounts[1];
-var SPECTRE_TEAM = web3.eth.accounts[1];
-var MANAGEMENT_LOCKED = web3.eth.accounts[2];
-var OPTION_POOL = web3.eth.accounts[3];
-
+var SpectreUtilityToken = artifacts.require("./SpectreUtilityToken.sol");
+var SpectreDividendToken = artifacts.require("./SpectreDividendToken.sol");
+var SpectreSubscriberToken = artifacts.require("./SpectreSubscriberToken.sol");
 
 //NB - confirm timings
-var presaleStart = 1509105600; //12am GMT 27th Oct
-var presaleEnd = 1509883200; //12am GMT 5th Nov
-var saleStart = 1510920000; //12am GMT 17th Nov
-var saleEnd = 1512907200; //12am GMT 10th Dec
-var discountSaleEnd = 1511524800; //9am GMT 24th Nov
+var presaleStart = 1509105600; //12pm GMT 27th Oct
+var presaleEnd = 1509883200; //12pm GMT 5th Nov
+var saleStart = 1510920000; //12pm GMT 17th Nov
+var saleEnd = 1512907200; //12pm GMT 10th Dec
+var discountSaleEnd = 1511524800; //12pm GMT 24th Nov
+
+//NB - set real addresses
+var OWNER = "0x09a568fd510741ae68e315d6d001a8d4b1682256";
+var WALLET = "0x670e095e92aff090fb8475131f7a8a5d98bd0155";
+var SPECTRE_TEAM = "0xdf20278eda5acf0d00e8a9162e60a35433cb070f";
+var MANAGEMENT_LOCKED = "0xcdb540d97dc828e855e87b8b3a3b546124e8d0e1";
+var OPTION_POOL = "0x8b04def77e45afb87d82a1e67675bbcc2566edd9";
 
 //Owner of below contracts will be truffle deployment address
 module.exports = async function(deployer) {
