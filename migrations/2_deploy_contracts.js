e54ee65ca209a6b9f5c2d3965b3527d6606b8934
Adam Dossa <adam.dossa@gmail.com>
2017-12-11 19:09:33 +0000

    Update owner

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -14,7 +14,7 @@ var SpectreSubscriber2Token = artifacts.require("./SpectreSubscriber2Token.sol")
 // var OPTION_POOL = web3.eth.accounts[3];
 
 // Previous values
-var OWNER = "0x09a568fd510741ae68e315d6d001a8d4b1682256";
+var OWNER = "0x3538b1a62f11d1fa03b0482dbf18d78cf9e79f47";
 var SPECTRE_TEAM = "0xdf20278eda5acf0d00e8a9162e60a35433cb070f";
 var MANAGEMENT_LOCKED = "0xcdb540d97dc828e855e87b8b3a3b546124e8d0e1";
 var OPTION_POOL = "0x8b04def77e45afb87d82a1e67675bbcc2566edd9";
