a472999444457a62a457132d1e6837afb4cd155e
Adam Dossa <adam.dossa@gmail.com>
2017-12-11 13:23:50 +0000

    Update to main net values

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,17 +8,16 @@ var SpectreSubscriber2Token = artifacts.require("./SpectreSubscriber2Token.sol")
 
 //NB - set real addresses
 
-var OWNER = web3.eth.accounts[0];
-var SPECTRE_TEAM = web3.eth.accounts[1];
-var MANAGEMENT_LOCKED = web3.eth.accounts[2];
-var OPTION_POOL = web3.eth.accounts[3];
+// var OWNER = web3.eth.accounts[0];
+// var SPECTRE_TEAM = web3.eth.accounts[1];
+// var MANAGEMENT_LOCKED = web3.eth.accounts[2];
+// var OPTION_POOL = web3.eth.accounts[3];
 
 // Previous values
-// var OWNER = "0x09a568fd510741ae68e315d6d001a8d4b1682256";
-// var WALLET = "0x670e095e92aff090fb8475131f7a8a5d98bd0155";
-// var SPECTRE_TEAM = "0xdf20278eda5acf0d00e8a9162e60a35433cb070f";
-// var MANAGEMENT_LOCKED = "0xcdb540d97dc828e855e87b8b3a3b546124e8d0e1";
-// var OPTION_POOL = "0x8b04def77e45afb87d82a1e67675bbcc2566edd9";
+var OWNER = "0x09a568fd510741ae68e315d6d001a8d4b1682256";
+var SPECTRE_TEAM = "0xdf20278eda5acf0d00e8a9162e60a35433cb070f";
+var MANAGEMENT_LOCKED = "0xcdb540d97dc828e855e87b8b3a3b546124e8d0e1";
+var OPTION_POOL = "0x8b04def77e45afb87d82a1e67675bbcc2566edd9";
 
 //Owner of below contracts will be truffle deployment address
 module.exports = async function(deployer) {
